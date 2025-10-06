d2dfc87102fd0cd234f09b546a70bbc84822b94c
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-10-06 13:50:48 +0200

    55

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent,NewTaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({required: true}) userId!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
 
   isAddingTask: boolean = false;
 
@@ -21,7 +21,8 @@ export class TasksComponent {
       id: 't1',
       userId: 'u1',
       title: 'Master Angular',
-      summary:'Learn all the basic and advanced features of Angular & how to apply them.',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
       dueDate: '2025-12-31',
     },
     {
@@ -41,20 +42,30 @@ export class TasksComponent {
     },
   ];
 
-  get selectedUserTasks(){
-    return this.tasks.filter(task => task.userId === this.userId)
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
 
-  onCompleteTask(id: string){
-    this.tasks = this.tasks.filter((task)=> task.id !== id);
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  onStartAddTask (){
+  onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask (){
+  onCancelAddTask() {
     this.isAddingTask = false;
   }
 
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+    this.isAddingTask = false;
+  }
 }
